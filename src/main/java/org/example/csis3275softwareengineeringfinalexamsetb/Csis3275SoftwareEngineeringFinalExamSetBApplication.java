8e4514ae1b68d60414734619ddb86eea0ce2b58e
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2024-08-10 13:04:52 -0700

    Initial commit

---
+++ src/main/java/org/example/csis3275softwareengineeringfinalexamsetb/Csis3275SoftwareEngineeringFinalExamSetBApplication.java
@@ -0,0 +1,13 @@
+package org.example.csis3275softwareengineeringfinalexamsetb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csis3275SoftwareEngineeringFinalExamSetBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Csis3275SoftwareEngineeringFinalExamSetBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/csis3275softwareengineeringfinalexamsetb/Csis3275SoftwareEngineeringFinalExamSetBApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.csis3275softwareengineeringfinalexamsetb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csis3275SoftwareEngineeringFinalExamSetBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
